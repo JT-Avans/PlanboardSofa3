373f1a8a34ea5fec4b9cd881403723ec6d331c14
Tony <tnd.lamquang@student.avans.nl>
2024-03-20 23:28:09 +0100

    Init template pattern

---
+++ Core.Domain/GitConnector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public abstract class GitConnector
+    {
+        public  void issueConverter()
+        {
+            authenticate();
+            convertIssuesToTickets();
+        }
+
+        void saveTickets(List<ITicket> list)
+        {
+
+        }
+
+        protected abstract void authenticate();
+        protected abstract void fetchIssues();
+        protected abstract void convertIssuesToTickets();
+    }
+}
---
+++ Core.Domain/GithubConnector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class GithubConnector : GitConnector
+    {
+        protected override void authenticate()
+        {
+            Console.WriteLine("authenticated to github");
+        }
+
+        protected override void convertIssuesToTickets()
+        {
+            fetchIssues();
+        }
+
+        protected override void fetchIssues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
