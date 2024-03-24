a40c9cee4d0007b4e0c6d20581a09d2296ea93fd
Tony <tnd.lamquang@student.avans.nl>
2024-03-24 15:02:10 +0100

    pipeline

---
+++ Core.Domain/Pipeline.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Pipeline
+    {
+        public void DevopPipeline()
+        {
+            Console.WriteLine("Retrieve source");
+            Console.WriteLine("Install packages");
+            Console.WriteLine("Building");
+            Console.WriteLine("Run tests");
+            Console.WriteLine("Deploy");
+        }
+    }
+}
